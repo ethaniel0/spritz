ac62930a57586b78e2026b55e7819dcae62df404
Ethaniel <ethanhorowitz@ymail.com>
2020-07-01 06:49:21 +0000

    save times

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
-const express = require('express');
+const express = require('express'),
+ fs = require('fs'),
+ bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
+app 
+	.engine('html', require('ejs').renderFile)
+	.set('views',  __dirname + '/public')
+	.set('view engine', 'html')
+
 app.get('/', (req, res) => {
-  res.sendFile('index');
+  res.render('index');
 });
 
 app.get('/set', (req, res) => {
-  res.sendFile(__dirname + '/public/set.html');
+	let rawdata = fs.readFileSync('time.json');
+	console.log(JSON.parse(rawdata))
+  res.render('set', JSON.parse(rawdata));
 });
 
+app.post('/set', (req, res) => {
+	let data = JSON.stringify(req.body);
+	fs.writeFileSync('time.json', data);
+	res.render('index');
+});
 
-app.post('get-time', (req, res) => {
-
+app.post('/get-time', (req, res) => {
+	let rawdata = fs.readFileSync('time.json');
+	let parts = JSON.parse(rawdata);
+	let toSend = parts.hour + ' ' + parts.minute + ' ' + parts.half;
+	res.send(toSend);
+	console.log(toSend);
 });
 
 app.listen(3000, () => {
